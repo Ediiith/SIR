a4efdd533dce03ddbdd51c24d99fe07b1c2aea49
ferard.julie@gmail.com <ferard.julie@gmail.com>
2017-03-15 15:41:10 +0100

    j'ai réglé le pb de chloé

--- src/accesBD/BDpersonnel.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE personnel (
-    idPersonnel VARCHAR(20),
-    mdp VARCHAR(20),
-    nomPersonnel VARCHAR(20),
-    prenomPersonnel VARCHAR(20),
-    metier VARCHAR(20),
-    PRIMARY KEY (idPersonnel)
-)
