7edd2cc1bc3191ee95929c42cf8b5ce5a9a05055
ferard.julie@gmail.com <ferard.julie@gmail.com>
2017-03-19 11:23:25 +0100

    enlever le dossier de chloé

--- Documents/NetBeansProjects/SIR/src/BD/BDpersonnel.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE personnel (
-    idPersonnel VARCHAR(20),
-    mdp VARCHAR(20),
-    nomPersonnel VARCHAR(20),
-    prenomPersonnel VARCHAR(20),
-    metier VARCHAR(30),
-    PRIMARY KEY (idPersonnel)
-)
